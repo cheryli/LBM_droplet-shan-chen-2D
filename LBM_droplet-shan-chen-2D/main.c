923d4cf1f67284c303d80df9d513cda1dd0c1de6
Li Chery <cheryli2017@outlook.com>
2020-03-27 10:44:52 +0800

    test

--- LBM_droplet-shan-chen-2D/main.c
+++ LBM_droplet-shan-chen-2D/main.c
@@ -1,13 +1,14 @@
 /*
 This LBM(lattice bolotz) program simulates a droplet imapct on a soild wall (2-dimensional) using shan-chen model.
 More information about LBM, refer to https://en.wikipedia.org/wiki/Lattice_Boltzmann_methods
-About shan-chen model, refer to 
---D2Q9 discrete velocity model is used -- 
+About shan-chen model, refer to https://doi.org/10.1103/PhysRevE.47.1815
+---- D2Q9 discrete velocity model is been used ---- 
 */
 
 #include <stdio.h>
 #include <math.h>
 
+// parameters
 int main()
 {
 	int a = 1, b = 1;
